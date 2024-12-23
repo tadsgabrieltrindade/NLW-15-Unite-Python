a4d1e79606e0d47b4102d063a89d36162d3e8fcd
tadsgabrieltrindade <icandoitgrt@gmail.com>
2024-12-22 21:14:30 -0300

    feat: implementação do flask com o seu servidor utilizando o blueprint

---
+++ src/main/server/server.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_cors import CORS
+from src.main.routes.events_routes import event_route_bp
+
+
+app = Flask(__name__)
+CORS(app)
+
+app.register_blueprint(event_route_bp)
+
